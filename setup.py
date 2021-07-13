277a493c9d4c682aec61452016b7a9bebf31c830
hauptmech <hauptmech@gmail.com>
2021-07-14 05:59:09 +0800

    Add package line back in

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['odio_urdf'],
     scripts=[],
+    package_dir={'odio_urdf':'odio_urdf'},
 )
 
 setup(**d)
