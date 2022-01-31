ee8cce0977bee8973806858f0a2502468b0ac979
M. Merdan <maierdan.maimaitimin@tadano.com>
2022-01-31 21:54:58 +0900

    use xml parser instead of catkin_pkg

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+import xml.etree.ElementTree as ET
 
-d = generate_distutils_setup(
-    packages=['odio_urdf'],
-    scripts=[],
-    package_dir={'odio_urdf':'odio_urdf'},
+pkg_info = ET.parse("package.xml")
+info = pkg_info.getroot()
+
+setup(
+    name="{}".format(info.find("name").text),
+    version="{}".format(info.find("version").text),
+    maintainer="{}".format(info.find("maintainer").text),
+    maintainer_email="{}".format(info.find("maintainer").attrib["email"]),
+    description="{}".format(info.find("description").text),
+    license="{}".format(info.find("license").text),
+    packages=["odio_urdf"],
+    package_dir={"odio_urdf": "odio_urdf"},
 )
-
-setup(**d)
